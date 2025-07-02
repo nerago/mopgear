1a4687433e23382829f98ac0254ca0c5b0875cd3
Nicholas Hardy <nicholas.h.hardy@gmail.com>
2025-07-02 23:36:29 +1000

    init order

--- src/main/java/au/nicholas/hardy/mopgear/ModelParams.java
+++ src/main/java/au/nicholas/hardy/mopgear/ModelParams.java
@@ -3,17 +3,16 @@ package au.nicholas.hardy.mopgear;
 import java.util.EnumMap;
 
 public class ModelParams {
-    static final EnumMap<Secondary, Integer> requiredAmounts = buildRequired();
     static final Secondary[] priority = new Secondary[]{Secondary.Haste, Secondary.Mastery, Secondary.Crit};
-//    static final Secondary[] reforgeTargets = new Secondary[]{Secondary.Hit, Secondary.Expertise, Secondary.Haste, Secondary.Mastery};
     static final Secondary[] reforgeTargets = new Secondary[]{Secondary.Hit, Secondary.Expertise, Secondary.Haste};
 
     static final double RATING_PER_PERCENT = 102.46;
     static final double TARGET_PERCENT = 7.5;
     static final int TARGET_RATING = (int) Math.ceil(RATING_PER_PERCENT * TARGET_PERCENT); // 768.485373827269
 
-    static final int PERMITTED_EXCEED = 50;
+    static final int PERMITTED_EXCEED = 100;
 
+    static final EnumMap<Secondary, Integer> requiredAmounts = buildRequired();
     private static EnumMap<Secondary, Integer> buildRequired() {
         EnumMap<Secondary, Integer> map = new EnumMap<>(Secondary.class);
         map.put(Secondary.Hit, TARGET_RATING);
